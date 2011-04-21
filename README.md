70bb99c9c52acbdab5956d03a022580850967eab
Michael Hall <mph@puddingbowl.org>
2011-04-21 16:39:20 -0700

    Added support for the official CATT output format while retaining the original remapping format (because it's easier for people to read).

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A simple script that reads in a spreadsheet of categories and their associated keywords and classifies the content it finds by checking hed, dek and body for occurrences of the keywords.
 
-If an article is deemed unclassifiable, the script logs it and then passes it through the summarize gem to look for interesting words, which are placed in a frequency table for further refinement of the keyword list
+If an article is deemed unclassifiable, the script logs it and then passes it through the summarize gem to look for interesting words, which are placed in a frequency table for further refinement of the keyword list.
 
 ## Setup
 
@@ -20,6 +20,8 @@ Some variables up front:
 
 You need an input spreadsheet set up as columns of categories (the first row) and keywords (each row after the first in a column). 
 
+The column heads should be the directory the content is being sorted into (e.g. "/security-tools" and not "Tools")
+
 Name the mapping file `#{site_abbreviation}_mappings.xls` 
 
 You also need the spreadsheet containing the articles. The script looks for the following values in these columns:
@@ -27,6 +29,7 @@ You also need the spreadsheet containing the articles. The script looks for the 
 * article id: column a
 * title: column b
 * deck: column c
+* current CDEV paths: column d
 * page number: column e
 * article text: column f
 
@@ -38,10 +41,11 @@ Both the article file and mapping file should go in the same directory as the sc
 
 Run the script. When it's done, look for `#{site_prefix}_remapped_articles.xls` in the directory.
 
-The spreadsheet should have three tabs:
+The spreadsheet should have four tabs:
 
 * Mappings: The list of articles with up to three mapped categories for each
 * No Hits: Articles that didn't have any keywords from any category
 * Candidates: A list of potentially interesting words from articles in the No Hits list
+* CATT Mappings: The official CATT migration format 
 
 Use the "Candidates" tab to refine the keywords and re-run the script. 
--- autotaxonomizer.rb
+++ autotaxonomizer.rb
@@ -7,10 +7,17 @@ require "readability"
 require "summarize"
 
 # set this to some value greater than 0 to limit output to n rows
-test = 50
+test = 0
+
 
 # to prefix the output (e.g. 'esp' for 'eSecurityPlanet')
 site_abbreviation = "esp"
+
+# to designate Old Site
+old_site = "http://esecurityplanet.com"
+
+# to designate CE site
+ce_site = "http://esecurityplanet.com"
 
 # array of words we don't want to count or find
 stop_words = ["security"]
@@ -39,6 +46,10 @@ freq_table_sheet = output_book.create_worksheet :name => "Candidates"
 # set up the title row for the output sheet
 output_sheet.row(0).concat ["CDEV ID","Title","Mapped Container 1","Mapped Container 2","Mapped Container 3"]
 
+# CATT remapping sheet
+catt_sheet = output_book.create_worksheet :name => "CATT mappings"
+
+catt_sheet.row(0).concat ["CDEV ID","Old Site / Directory","CE Site","CE Container"]
 
 # create the hash we'll use to store each category and its keywords
 categories = {}
@@ -83,15 +94,17 @@ freq_table = Hash.new(0)
 
 output_row = 1
 nohit_row = 0
+catt_sheet_row = 1
+
 
 article_sheet.each 1 do |row|
 
   # might want to test, so let's make it easy to kill output before a complete run:
 
-  if test > 0 
+  if test > 0
     break if output_row > test
   end
-  
+
   # we want to keep track of the number of categories we've found and stop at 3
   hits = 0
 
@@ -99,11 +112,17 @@ article_sheet.each 1 do |row|
   next if row[4] > 1
 
   # downcasing the title, deck and body will make this easier
-  id = row[0].to_i
+  cdev_id = row[0].to_i
   title = row[1].to_s.downcase
   deck = row[2].to_s.downcase
 
-  puts title
+  # the dump has each directory ("path") the article is currently found in
+  paths = row[3].split(",")
+
+  # clean out the whitespace
+  paths.each {|p| p.strip! }
+
+  #  puts title
 
   # sanitize the markup in the body
   cleaned_text = Sanitize.clean(Readability::Document.new(row[5]).content)
@@ -123,7 +142,7 @@ article_sheet.each 1 do |row|
   # put our elements in an array so we can loop through them with just one function
   elements = [title,deck,text]
 
-  output_sheet.row(output_row)[0] = id
+  output_sheet.row(output_row)[0] = cdev_id
   output_sheet.row(output_row)[1] = title
 
   # keep track of which categories we have hits for, so we don't repeat them
@@ -150,30 +169,53 @@ article_sheet.each 1 do |row|
       end
     end
 
-    # if we don't find keywords from any categories, the hit counter never goes above 0,
-    # in that case, we want to log the non-hit article for review so we can refine the
-    # list of keywords
 
-    if hits == 0
-      i = 1
-      nohit_sheet.row(nohit_row)[0] = title
-      nohit_row +=1
-      possible_topics.each do |pt|
-        freq_table[pt] += 1
+   
+
+  end
+
+ # write out whatever we found in the official CATT format to the catt_sheet
+
+  unless hit_cats.size == 0
+
+    paths.each do |path|
+      hit_cats.each do |category|
+        catt_sheet.row(catt_sheet_row).concat [cdev_id,"#{old_site}#{path}",ce_site,category]
+        catt_sheet_row +=1
       end
     end
   end
+
+
+  # if we don't find keywords from any categories, the hit counter never goes above 0,
+  # in that case, we want to log the non-hit article for review so we can refine the
+  # list of keywords
+
+  if hits == 0
+    i = 1
+    nohit_sheet.row(nohit_row)[0] = title
+    nohit_row +=1
+    possible_topics.each do |pt|
+      freq_table[pt] += 1
+    end
+  end
+
   # move on to the next row
   output_row += 1
 end
 
+
+
+# generate the word candidates tab (sorting the hash of likely keywords)
 freq_row = 0
 
-freq_table.each do |k,v|
-  freq_table_sheet.row(freq_row)[0] = k
-  freq_table_sheet.row(freq_row)[1] = v
+freq_table.sort{|k,v| v[1]<=>k[1]}.each { |elem|
+  freq_table_sheet.row(freq_row)[0] = elem[0]
+  freq_table_sheet.row(freq_row)[1] = elem[1]
   freq_row +=1
-end
+}
+
+
 
 
 # write the spreadsheet
